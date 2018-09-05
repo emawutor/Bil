a7ab1d91e222784a596fecf767ce4b0fc127cd68
Emmanuel Mawutor <emawutor@indiana.edu>
2018-09-05 15:41:10 +0100

    created show and index pages

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,42 @@
+require 'json'
+require 'open-uri'
 class StocksController < ApplicationController
+  def financials
+    @financials = Stock.all.where(sector: "Financials")
+  end
+  def financials_show
+    @financial = Stock.find(params[:id])
+    @company = JSON.parse(open("https://api.iextrading.com/1.0/stock/#{@financial.ticker}/company").read)
+    @news = JSON.parse(open("https://api.iextrading.com/1.0/stock/#{@financial.ticker}/news").read)
+  end
+  def utilities
+    @utilities = Stock.all.where(sector: "Utilities")
+  end
+  def consumer_discretionary
+    @consumer_discretionary = Stock.all.where(sector: "Consumer Discretionary")
+  end
+  def consumer_staples
+    @consumer_staples = Stock.all.where(sector: "Consumer Staples")
+  end
+  def energy
+    @energy = Stock.all.where(sector: "Energy")
+  end
+  def healthcare
+    @healthcare = Stock.all.where(sector: "Healthcare")
+  end
+  def industrials
+    @industrials = Stock.all.where(sector: "Industrials")
+  end
+  def tech
+    @tech = Stock.all.where(sector: "Technology")
+  end
+  def telecom
+    @telecom = Stock.all.where(sector: "Telecom")
+  end
+  def materials
+    @materials = Stock.all.where(sector: "Materials")
+  end
+  def real_estate
+    @real_estate = Stock.all.where(sector: "Real Estate")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'iexapi/collection'
   devise_for :users
   root to: 'pages#home'
+  resources :stocks, only: [:financials, :financials_show]
+  get "financials", to: "stocks#financials", as: "financials"
+  get "financials/:id", to: "stocks#financials_show", as: "stock"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
