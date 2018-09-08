81c79bf34089ee65bcfc3c3ec26a8f0e88ac04de
emmanuel mawutor <emawutor@indiana.edu>
2018-09-07 21:22:56 -0600

    added a sector api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @sectors= JSON.parse(open("https://api.iextrading.com/1.0/stock/market/sector-performance").read)
+    raise
   end
 end
